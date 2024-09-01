63a86da1e5605a43ce3fb5b00c5ae1e5b7e80393
mynameco <my.name.post@gmail.com>
2024-09-02 00:17:18 +0300

    fix

--- Invaders/Invaders/Program.cs
+++ Invaders/Invaders/Program.cs
@@ -213,7 +213,7 @@ for (int i = m.Length - 1; i >= 0; i--)
 		if (m[indexInput] == 'd' &&
 			m[indexPlayer] < p - 7)
 		{
-			m[i] = (m[i] == ' ' || m[i] == '#' || m[i] == '^') && (m[i + lookRight] == '#' || m[i + lookRight] == '^') ? m[i + lookRight] : m[i];
+			m[i] = (m[i] == ' ' || m[i] == '#' || m[i] == '^') && (m[i + lookRight] == '#' || m[i + lookRight] == '^') && m[i] != m[i + lookRight] ? m[i + lookRight] : m[i];
 			if (m[i] != ch)
 				continue;
 
@@ -228,7 +228,7 @@ for (int i = m.Length - 1; i >= 0; i--)
 			if ((m[indexCycleMove] % 18) > 0 &&
 				(m[indexCycleMove] % 18) < 9)
 			{
-				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookRight] >= '*' && m[i + lookRight] <= '\\' ? m[i + lookRight] : m[i];
+				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookRight] >= '*' && m[i + lookRight] <= '\\' && m[i] != m[i + lookRight] ? m[i + lookRight] : m[i];
 				if (m[i] != ch)
 					continue;
 
@@ -241,7 +241,7 @@ for (int i = m.Length - 1; i >= 0; i--)
 			if ((m[indexCycleMove] % 18) == 9 ||
 				(m[indexCycleMove] % 18) == 0)
 			{
-				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookDown] >= '*' && m[i + lookDown] <= '\\' ? m[i + lookDown] : m[i];
+				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookDown] >= '*' && m[i + lookDown] <= '\\' && m[i] != m[i + lookDown] ? m[i + lookDown] : m[i];
 				if (m[i] != ch)
 					continue;
 
@@ -301,7 +301,7 @@ for (int i = 0; i < m.Length; i++)
 		if (m[indexInput] == 'a' &&
 			m[indexPlayer] > 5)
 		{
-			m[i] = (m[i] == ' ' || m[i] == '#' || m[i] == '^') && (m[i + lookLeft] == '#' || m[i + lookLeft] == '^') ? m[i + lookLeft] : m[i];
+			m[i] = (m[i] == ' ' || m[i] == '#' || m[i] == '^') && (m[i + lookLeft] == '#' || m[i + lookLeft] == '^') && m[i] != m[i + lookLeft] ? m[i + lookLeft] : m[i];
 			if (m[i] != ch)
 				continue;
 
@@ -315,7 +315,7 @@ for (int i = 0; i < m.Length; i++)
 			// Сдвиг врагов влево
 			if ((m[indexCycleMove] % 18) > 9)
 			{
-				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookLeft] >= '*' && m[i + lookLeft] <= '\\' ? m[i + lookLeft] : m[i];
+				m[i] = (m[i] == ' ' || (m[i] >= '*' && m[i] <= '\\')) && m[i + lookLeft] >= '*' && m[i + lookLeft] <= '\\' && m[i] != m[i + lookLeft] ? m[i + lookLeft] : m[i];
 				if (m[i] != ch)
 					continue;
 
