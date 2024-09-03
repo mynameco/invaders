a574a5d7e2981982e269b400167dcca706565d97
mynameco <my.name.post@gmail.com>
2024-09-03 23:04:08 +0300

    update net

--- Invaders/InvadersCompactFramework/Properties/AssemblyInfo.cs
+++ Invaders/InvadersCompactFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InvadersCompactOld")]
+[assembly: AssemblyTitle("InvadersCompactFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InvadersCompactOld")]
+[assembly: AssemblyProduct("InvadersCompactFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9cbc8235-93ee-4921-bba9-586db350d098")]
+[assembly: Guid("0cad65d2-977f-4236-9d5a-59246840837c")]
 
 // Version information for an assembly consists of the following four values:
 //
